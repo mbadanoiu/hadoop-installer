e713d210855811d5badb2f8e2a98a5a122193b36
mbadanoiu <mbadanoiu@yahoo.com>
2017-04-22 17:20:27 +0300

    Fixes and additions

--- setup_hadoop.sh
+++ setup_hadoop.sh
@@ -1,26 +1,45 @@
+#! /bin/bash
+
 ##############################DEPENDENCIES################################
 if ! type ssh > /dev/null 2>&1 || ! type ssh-keygen > /dev/null 2>&1; then
-	echo -e "\n\e[31mInstall \"openssl\" then come back\e[0m\n"
+	/bin/echo -e "\n\e[31mInstall \"openssl\" then come back\e[0m\n"
 	exit
 fi
 if [[ ! -d /usr/lib/jvm/default-java ]]; then
-	echo -e "\n\e[31mInstall \"openjdk\" then come back\e[0m\n"
+	if [[ ! -d /usr/lib/jvm/java-*-openjdk-* ]]; then
+		echo -e "\n\e[31mInstall \"openjdk\" then come back\e[0m\n"
+	else
+		echo -e "\n\e[31mCreate \"/usr/lib/jvm/default-java\" symlink pointing to \"/usr/lib/jvm/java-*-openjdk-*\" directory\e[0m\n"
+	fi
 	exit
 fi
 if ! type wget > /dev/null 2>&1; then
-	echo -e "\n\e[31mInstall \"wget\" then come back\e[0m\n"
+	/bin/echo -e "\n\e[31mInstall \"wget\" then come back\e[0m\n"
+	exit
+fi
+if ! type jps > /dev/null 2>&1; then
+	/bin/echo -e "\n\e[31mInstall \"openjdk-*-jdk-headless\" then come back\e[0m\n"
 	exit
 fi
 ##########################################################################
 
 sudo addgroup hadoop
 sudo adduser --ingroup hadoop hduser
-mkdir /home/hduser/.ssh
+sudo -u hduser mkdir /home/hduser/.ssh
 sudo -u hduser ssh-keygen -f /home/hduser/.ssh/hduser_rsa -t rsa -P ""
 cat /home/hduser/.ssh/hduser_rsa.pub >> /home/hduser/.ssh/authorized_keys
 chown -R hduser:hadoop /home/hduser/.ssh
-sudo service ssh restart
+
+if ! sudo service ssh restart 2> /dev/null; then
+	sudo systemctl restart ssh 2> /dev/null
+fi	
+
 wget http://mirror.evowise.com/apache/hadoop/core/hadoop-2.7.3/hadoop-2.7.3.tar.gz  ###current stable, should change with time
+if (( 0 != $? )); then
+	/bin/echo -e "\n\e[31mCouldn't download archive.\nCheck if:\n\t1) You are connected 2 the internet\n\t2) Check if site \"http://mirror.evowise.com/\" is up\n\t3) You have permissions to write to the current directory\e[0m\n"
+	exit
+fi
+
 echo "Extracting Hadoop, might take a while ..."
 tar -xzf hadoop-2.7.3.tar.gz ###
 echo "Extraction done"
@@ -71,7 +90,7 @@ sed -i '/<configuration>/,/<\/configuration>/c\
 <\/property>\
 \
 <property>\
-  <name>fs.defaultFS<\/name>\
+  <name>fs.default.name<\/name>\
   <value>hdfs\:\/\/localhost\:54310<\/value>\
   <description>The name of the default file system.  A URI whose\
   scheme and authority determine the FileSystem implementation.  The\
@@ -112,24 +131,24 @@ sed -i '/<configuration>/,/<\/configuration>/c\
 <\/configuration>' /usr/local/hadoop/etc/hadoop/hdfs-site.xml
 ##################################################################################
 
-/usr/local/hadoop/bin/hdfs namenode -format
-echo -e "\n\e[32mStarting up hadoop server...\e[0m\n"
+sudo -u hduser /usr/local/hadoop/bin/hdfs namenode -format
+/bin/echo -e "\n\e[32mStarting up hadoop server...\e[0m\n"
 sudo -u hduser /usr/local/hadoop/sbin/start-dfs.sh
 services=$(jps)
 if [[ $services == *'NameNode'* ]] && [[ $services == *'DataNode'* ]] && [[ $services == *'SecondaryNameNode'* ]]; then
-	echo -e "\n\e[32mAll seems to be running well\e[0m\n"
+	/bin/echo -e "\n\e[32mAll seems to be running well\e[0m\n"
 else
-	echo -e "\n\e[31mWell something went wrong with the startup\e[0m\n"
+	/bin/echo -e "\n\e[31mWell something went wrong with the startup\e[0m\n"
 	echo $(jps)
 	exit
 fi
 
-echo -e "\n\e[32mStopping hadoop server...\e[0m\n"
+/bin/echo -e "\n\e[32mStopping hadoop server...\e[0m\n"
 sudo -u hduser /usr/local/hadoop/sbin/stop-dfs.sh
 services=$(jps)
 if [[ $services == *'NameNode'* ]] || [[ $services == *'DataNode'* ]] || [[ $services == *'SecondaryNameNode'* ]]; then
-	echo -e "\n\e[31mThings seem to be running still\e[0m\n"
+	/bin/echo -e "\n\e[31mThings seem to be running still\e[0m\n"
 	echo $(jps)
 else
-	echo -e "\n\e[32mHadoop stopped gracefully\e[0m\n"
+	/bin/echo -e "\n\e[32mHadoop stopped gracefully\e[0m\n"
 fi
